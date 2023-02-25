60b6affdf9108e7bf52adfe9d23c847f4586196a
GomezJR92 <n.albertogomez@gmail.com>
2023-02-25 12:09:59 -0300

    metodos

--- Proyecto-ByteBank/src/Cuenta.java
+++ Proyecto-ByteBank/src/Cuenta.java
@@ -1,9 +1,12 @@
 // Entidad cuenta:	
-	class cuenta{
-		
-		double saldo;	
-		int agencia;
-		int numero;
-		String titular;
+class cuenta {
+
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+
+	void depositar(double valor) {
+
 	}
-	
+}
---
+++ Proyecto-ByteBank/src/PruebaMetodos.java
@@ -0,0 +1,12 @@
+
+public class PruebaMetodos {
+	
+	public static void main(String[] args) {
+		
+		cuenta cuentaDeNicolas = new cuenta();
+		
+		cuentaDeNicolas.saldo = 300;
+		cuentaDeNicolas.depositar(300);
+	}
+
+}
